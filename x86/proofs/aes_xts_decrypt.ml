73ed7b900350396f9abf7eccab477278308ceb01
Yan Peng <yppe@amazon.com>
2025-02-26 23:16:36 +0000

    Add aes-xts assembly

---
+++ x86/proofs/aes_xts_decrypt.ml
@@ -0,0 +1,8 @@
+(*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0 OR ISC OR MIT-0
+ *)
+
+needs "x86/proofs/base.ml";;
+
+print_coda_from_elf 0x9c4 "x86/aes-xts/aesni-x86_64-decrypt.o";;
---
+++ x86/proofs/aes_xts_encrypt.ml
@@ -0,0 +1,8 @@
+(*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0 OR ISC OR MIT-0
+ *)
+
+needs "x86/proofs/base.ml";;
+
+print_coda_from_elf 0x960 "x86/aes-xts/aesni-x86_64-encrypt.o";;
