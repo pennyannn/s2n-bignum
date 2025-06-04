fd647b2a5561688ccedcae00a731d6fe06b0da47
manastasova <manastasova2017@fau.edu>
2025-06-04 20:25:58 +0000

    Min example for loop with counter

--- x86/proofs/mlkem_keccak_f1600_loop_invar.ml
+++ x86/proofs/mlkem_keccak_f1600_loop_invar.ml
@@ -9,32 +9,32 @@
   Proving a mlkem_keccak_f1600 property about program 'mlkem_keccak_f1600.S'
 ******************************************************************************)
 (* When there is a table *)
-(**** print_coda_from_elf (-1) "x86/mlkem/mlkem_keccak_f1600_loop_invar_4.o";;
+(**** print_coda_from_elf (-1) "x86/mlkem/mlkem_keccak_f1600_loop_invar_11.o";;
  ****)
 
 (* When there is no table *)
  (**** print_literal_from_elf "x86/mlkem/mlkem_keccak_f1600_loop_invar.o";;
  ****)
 
-let mlkem_keccak_f1600_mc_loop_invar_4 = define_assert_from_elf
-  "mlkem_keccak_f1600_mc_loop_invar_4" "x86/mlkem/mlkem_keccak_f1600_loop_invar.o"         
+let mlkem_keccak_f1600_mc_loop_invar_11 = define_assert_from_elf
+  "mlkem_keccak_f1600_mc_loop_invar_11" "x86/mlkem/mlkem_keccak_f1600_loop_invar.o"         
 [
   0x49; 0x89; 0xf7;        (* MOV (% r15) (% rsi) *)
   0x49; 0xc7; 0xc4; 0x00; 0x00; 0x00; 0x00;
                            (* MOV (% r12) (Imm32 (word 0)) *)
   0x4d; 0x8d; 0x7f; 0x08;  (* LEA (% r15) (%% (r15,8)) *)
   0x49; 0x83; 0xc4; 0x01;  (* ADD (% r12) (Imm8 (word 1)) *)
-  0x49; 0xf7; 0xc4; 0x18; 0x00; 0x00; 0x00;
-                           (* TEST (% r12) (Imm32 (word 24)) *)
-  0x75; 0xef;              (* JNE (Imm8 (word 239)) *)
+  0x49; 0x83; 0xfc; 0x18;  (* CMP (% r12) (Imm8 (word 24)) *)
+  0x75; 0xf2;              (* JNE (Imm8 (word 242)) *)
   0xc3                     (* RET *)
 ];;
 
+(* 
+ let mlkem_keccak_f1600_loop_invar_11 = define_trimmed "mlkem_keccak_f1600_loop_invar_11" mlkem_keccak_f1600_mc_loop_invar_11;;
 
- let mlkem_keccak_f1600_loop_invar_4 = define_trimmed "mlkem_keccak_f1600_loop_invar_4" mlkem_keccak_f1600_mc_loop_invar_4;;
+ let MLKEM_KECCAK_F1600_EXEC_11 = X86_MK_EXEC_RULE mlkem_keccak_f1600_loop_invar_11;; *)
 
- let MLKEM_KECCAK_F1600_EXEC = X86_MK_EXEC_RULE mlkem_keccak_f1600_loop_invar_4;;
-
+ let MLKEM_KECCAK_F1600_EXEC_11 = X86_MK_EXEC_RULE mlkem_keccak_f1600_mc_loop_invar_11;;
 
 (* let MLKEM_KECCAK_F1600_SPEC = prove(
   `forall pc:num stackpointer:int64 bitstate:int64 returnaddress.
@@ -42,7 +42,7 @@ let mlkem_keccak_f1600_mc_loop_invar_4 = define_assert_from_elf
        nonoverlapping_modulo (2 EXP 64) (pc, 0x344) (val (word_add bitstate (word 0):int64),200)
    ==> ensures x86
   // Precondition
-  (\s. bytes_loaded s (word pc) mlkem_keccak_f1600_loop_invar_4 /\
+  (\s. bytes_loaded s (word pc) mlkem_keccak_f1600_loop_invar_11 /\
        read RIP s = word pc /\
        read RSP s = stackpointer /\
        read (memory :> bytes64 stackpointer) s = returnaddress /\
@@ -58,14 +58,13 @@ let mlkem_keccak_f1600_mc_loop_invar_4 = define_assert_from_elf
   `forall pc:num stackpointer:int64 returnaddress.
       ensures x86
   // Precondition
-  (\s. bytes_loaded s (word pc) mlkem_keccak_f1600_loop_invar_4 /\
+  (\s. bytes_loaded s (word pc) mlkem_keccak_f1600_mc_loop_invar_11 /\
        read RIP s = word pc /\
        read RSP s = stackpointer /\
        read (memory :> bytes64 stackpointer) s = returnaddress)
   // Postcondition
   (\s.  read RIP s = returnaddress /\
-         read RSP s = word_add stackpointer (word 8)/\
-         read R12 s = word 24)
+        read RSP s = word_add stackpointer (word 8))
   (MAYCHANGE [RIP;RSP;RAX;RBX;RCX;RDX;RBP;R8;R9;R10;R11;R12;R13;R14;R15;RDI;RSI] ,, MAYCHANGE SOME_FLAGS)`
   ,
 
@@ -85,7 +84,7 @@ let mlkem_keccak_f1600_mc_loop_invar_4 = define_assert_from_elf
     `0` (* counter begin number *)
     `24` (* counter end number *)
     `pc + 0xa` (* loop body start PC *)
-    `pc + 0x19` (* loop backedge branch PC *)
+    `pc + 0x16` (* loop backedge branch PC *)
     `\i s. // loop invariant at the end of the iteration
            (read R12 s = word i /\
             read RSP s = stackpointer /\
@@ -101,27 +100,40 @@ let mlkem_keccak_f1600_mc_loop_invar_4 = define_assert_from_elf
     (* entrance to the loop *)
     (* Let's use X86_SIM_TAC which is ENSURES_INIT_TAC + X86_STEPS_TAC +
        ENSURES_FINAL_STATE_TAC + some post-processing. *)
-    X86_SIM_TAC MLKEM_KECCAK_F1600_EXEC (1--2) THEN
-    REPEAT CONJ_TAC THEN
-    CONV_TAC WORD_RULE;
+    X86_SIM_TAC MLKEM_KECCAK_F1600_EXEC_11 (1--2);
 
-    (* The loop body. let's prove this later. *)
-    (* If you are interactively exploring this proof, try `r 1;;`. *)
-    ALL_TAC;
 
-    (* Prove that backedge is taken if i != 10. *)
+    (* the body of the loop *)
     REPEAT STRIP_TAC THEN
-    X86_SIM_TAC EXEC [1];
+    X86_SIM_TAC MLKEM_KECCAK_F1600_EXEC_11 (1--3) THEN
+     REPEAT CONJ_TAC THENL [
+
+      CONV_TAC WORD_RULE;
+
+       REWRITE_TAC [WORD_BLAST `word_add x (word 18446744073709551593):int64 =
+                             word_sub x (word 23)`] THEN
+
+      REWRITE_TAC[VAL_WORD_SUB_EQ_0] THEN
+      REWRITE_TAC[VAL_WORD;DIMINDEX_64] THEN
+
+      IMP_REWRITE_TAC[MOD_LT; ARITH_RULE`23 < 2 EXP 64`] THEN
+
+      CONJ_TAC THENL [ (* will create two arithmetic subgoals. *)
+      UNDISCH_TAC `i < 24` 
+      THEN ARITH_TAC;
+      ARITH_TAC
+      ]];
+
+    (* Prove that backedge is taken if i != 24. *)
+    REPEAT STRIP_TAC THEN
+    X86_SIM_TAC MLKEM_KECCAK_F1600_EXEC_11 [1];
 
     (* Loop exit to the end of the program *)
-    X86_SIM_TAC EXEC (1--2) THEN
+    X86_SIM_TAC MLKEM_KECCAK_F1600_EXEC_11 (1--2) THEN
     (* word (10*2) = word 20 *)
-    CONV_TAC WORD_RULE
-  ] 
-    (* counter begin < counter end *)
-    ARITH_TAC;
+  ]);
 
-
+(* 
   ENSURES_INIT_TAC "s0" THEN
   REPEAT STRIP_TAC THEN
 
@@ -150,4 +162,4 @@ let mlkem_keccak_f1600_mc_loop_invar_4 = define_assert_from_elf
   ENSURES_FINAL_STATE_TAC THEN
 
   ASM_REWRITE_TAC[]
-  );;
+  );; *)
